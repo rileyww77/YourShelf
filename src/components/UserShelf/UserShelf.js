a4f051dda36861cad14b34b60533a5864559ba92
riley_ww77 <riley_ww77@yahoo.com>
2020-09-08 14:13:30 -0500

    added all components with base wireframes in each. database pulled into pool.js

---
+++ src/components/UserShelf/UserShelf.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+
+class UserShelf extends Component {
+  state = {
+    heading: 'Class Component',
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.heading}</h2>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(UserShelf);
