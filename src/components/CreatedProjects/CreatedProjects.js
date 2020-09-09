2bacd1939b0be93498b02e7be23d642219b4dd20
riley_ww77 <riley_ww77@yahoo.com>
2020-09-09 11:38:43 -0500

    displaying only user projects on my projects page. still need to add edit button

--- src/components/CreatedProjects/CreatedProjects.js
+++ src/components/CreatedProjects/CreatedProjects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import AddProjectButton from '../AddProjectButton/AddProjectButton';
+import UserProjectList from '../UserProjectList/UserProjectList'
 
 
 class CreatedProjects extends Component {
@@ -23,7 +24,13 @@ class CreatedProjects extends Component {
         <h2>{this.state.heading}</h2>
         <AddProjectButton />
 
-        
+        {this.props.store.userProjects.map((project) => {
+            return (
+                <div key={project.name}>
+                    <UserProjectList project={project} />
+                </div>
+            )
+        })}
       </div>
     );
   }
